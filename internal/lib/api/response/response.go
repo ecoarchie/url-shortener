cf245a14f3d7b6282f1a2afeace4a23606c9e067
ecoarchie <eco.artur@gmail.com>
2024-10-27 22:45:25 +0300

    add: functional tests

--- internal/lib/api/response/response.go
+++ internal/lib/api/response/response.go
@@ -38,7 +38,7 @@ func ValidationError(errs validator.ValidationErrors) Response {
 		case "required":
 			errMsgs = append(errMsgs, fmt.Sprintf("field %s is required", err.Field()))
 		case "url":
-			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not valid url", err.Field()))
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not a valid URL", err.Field()))
 		default:
 			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not valid", err.Field()))
 		}
---
+++ tests/url_shortener_test.go
@@ -0,0 +1,121 @@
+package tests
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/ecoarchie/url-shortener/internal/http-server/handlers/url/save"
+	"github.com/ecoarchie/url-shortener/internal/lib/api"
+	"github.com/ecoarchie/url-shortener/internal/lib/random"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	host = "localhost:8085"
+)
+
+func TestURLShortener_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	e.POST("/url").
+		WithJSON(save.Request{
+			URL:   gofakeit.URL(),
+			Alias: random.RandomString(10),
+		}).
+		WithBasicAuth("testuser", "testpassword").
+		Expect().
+		Status(200).
+		JSON().Object().
+		ContainsKey("alias")
+}
+
+//nolint:funlen
+func TestURLShortener_SaveRedirect(t *testing.T) {
+	testCases := []struct {
+		name  string
+		url   string
+		alias string
+		error string
+	}{
+		{
+			name:  "Valid URL",
+			url:   gofakeit.URL(),
+			alias: gofakeit.Word() + gofakeit.Word(),
+		},
+		{
+			name:  "Invalid URL",
+			url:   "invalid_url",
+			alias: gofakeit.Word(),
+			error: "field URL is not a valid URL",
+		},
+		{
+			name:  "Empty Alias",
+			url:   gofakeit.URL(),
+			alias: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			u := url.URL{
+				Scheme: "http",
+				Host:   host,
+			}
+
+			e := httpexpect.Default(t, u.String())
+
+			// Save
+
+			resp := e.POST("/url").
+				WithJSON(save.Request{
+					URL:   tc.url,
+					Alias: tc.alias,
+				}).
+				WithBasicAuth("testuser", "testpassword").
+				Expect().Status(http.StatusOK).
+				JSON().Object()
+
+			if tc.error != "" {
+				resp.NotContainsKey("alias")
+
+				resp.Value("error").String().IsEqual(tc.error)
+
+				return
+			}
+
+			alias := tc.alias
+
+			if tc.alias != "" {
+				resp.Value("alias").String().IsEqual(tc.alias)
+			} else {
+				resp.Value("alias").String().NotEmpty()
+
+				alias = resp.Value("alias").String().Raw()
+			}
+
+			// Redirect
+
+			testRedirect(t, alias, tc.url)
+		})
+	}
+}
+
+func testRedirect(t *testing.T, alias string, urlToRedirect string) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+		Path:   alias,
+	}
+
+	redirectedToURL, err := api.GetRedirect(u.String())
+	require.NoError(t, err)
+
+	require.Equal(t, urlToRedirect, redirectedToURL)
+}
